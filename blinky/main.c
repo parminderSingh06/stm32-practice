ebdb49a5d8510bfb756227fec93319bdf6f1c242
Parminder Singh <parmindersingh1086@gmail.com>
2025-09-21 17:49:03 -0400

    Changed code to be organized via structs

--- blinky/main.c
+++ blinky/main.c
@@ -1,42 +1,44 @@
-#define PERIPHERAL_BASE (0x40000000)
+typedef struct 
+{
+    volatile uint32_t MODER;    /*offset: 0x00      */
+    volatile uint32_t OTYPER;   /*offset: 0x04      */
+    volatile uint32_t OSPEEDR;  /*offset: 0x08      */
+    volatile uint32_t PUPDR;    /*offset: 0x0C      */
+    volatile uint32_t IDR;      /*offset: 0x10      */
+    volatile uint32_t ODR;      /*offset: 0x14      */
+    volatile uint32_t BSRR;     /*offset: 0x18      */
+    volatile uint32_t LCKR;     /*offset: 0x1C      */
+    volatile uint32_t AFRL;     /*offset: 0x20     */    
+    volatile uint32_t AFRH;     /*offset: 0x24      */
+} GPIO_TypeDef;
 
-#define AHB1_OFFSET (0x00020000)
+typedef struct
+{
+    volatile uint32_t DUMMY[12];
+    volatile uint32_t AHB1ENR;      /*offset: 0x30*/ 
+} RCC_TypeDef;
 
-#define AHB1_BASE (PERIPHERAL_BASE + AHB1_OFFSET)
+#define RCC_BASE    0x40023800
+#define GPIOA_BASE  0x40020000
 
-#define GPIOA_OFFSET (0x0000UL)
+#define RCC     ((RCC_TypeDef*) RCC_BASE)
+#define GPIOA   ((GPIO_TypeDef*)GPIOA_BASE)
 
-#define GPIOA_BASE (AHB1_BASE + GPIOA_OFFSET)
+#define GPIOAEN     (1U<<0)
 
-#define RCC_OFFSET (0x3800UL)
+#define PIN5        (1U<<5)
 
-#define RCC_BASE (AHB1_BASE + RCC_OFFSET)
-
-#define AHB1ENR_OFFSET (0X30UL)
-
-#define RCC_AHB1ENR_R (*(volatile unsigned int *) (RCC_BASE + AHB1ENR_OFFSET))
-
-#define MODE_R_OFFSET (0x00UL)
-
-#define GPIOA_MODE_R (*(volatile unsigned int *) (GPIOA_BASE + MODE_R_OFFSET))
-
-#define OD_R_OFFSET (0x14)
-
-#define GPIOA_OD_R (*(volatile unsigned int *) (GPIOA_BASE + OD_R_OFFSET))
-
-#define PIN5 (1U << 5)
-
-#define LED_PIN (PIN5)
+#define LED_PIN     PIN5
 
 int main(void){
-    RCC_AHB1ENR_R |= (1U << 0);
+    RCC->AHB1ENR |= GPIOAEN;
 
-    GPIOA_MODE_R &= ~(1U << 11);
-    GPIOA_MODE_R |= (1U << 10);
+    GPIOA->MODER &= ~(1U<<11);
+    GPIOA->MODER |= (1U<<10);
 
     while(1){
-        GPIOA_OD_R ^= LED_PIN;
-        for (int i = 0; i < 100000; i++){}
-        
+        GPIOA->ODR ^= LED_PIN;
+
+        for(int i=0;i<200000;i++){}
     }
 }
